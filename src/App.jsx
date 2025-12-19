813a5d5cf64aaa0eac433f70414b0f9b186d026d
priyanshuraj.debug <rajpriyanshu964@gmail.com>
2025-12-19 14:07:00 +0530

    refactor: clean up imports and improve loading state handling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
 import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { Outlet } from "react-router-dom"
 import authService from "./appwrite/auth"
 import { login, logout } from "./store/authSlice"
 import { Footer, Header } from "./components/index"
-import { Outlet } from "react-router"
 import Loader from "./components/Loader"
-import { useSelector } from "react-redux"
+
 function App() {
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
-const loader = useSelector((state) => state.ui.loading)
+  const loader = useSelector((state) => state.ui.loading)
+
   useEffect(() => {
     authService.getCurrentUser()
       .then((userData) => {
-        if (userData) {
-          dispatch(login( userData ))
-        } else {
-          dispatch(logout())
-        }
+        if (userData) dispatch(login(userData))
+        else dispatch(logout())
       })
       .finally(() => setLoading(false))
-  }, [])
+  }, [dispatch])
 
-  return !loading ? (
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-slate-950">
+        <Loader />
+      </div>
+    )
+  }
+
+  return (
     <div className="min-h-screen flex flex-col bg-slate-950 text-slate-100">
       {loader && <Loader />}
       <Header />
 
-      {/* 👇 main will expand only when content grows */}
       <main className="flex-1">
         <Outlet />
       </main>
 
       <Footer />
-
     </div>
-  ) : null
+  )
 }
 
 export default App
